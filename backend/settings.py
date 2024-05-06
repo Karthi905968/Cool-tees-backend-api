9b46418a3b0fc5295d2ce8c4d79086b976aab947
TECH I.S <techi.s.@TECHs-MacBook-Air.local>
2024-05-06 10:37:41 +0530

    removed cors-headers

--- backend/settings.py
+++ backend/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'backend.cors_middleware.CorsMiddleware', 
 ]
 
 ROOT_URLCONF = 'backend.urls'
